62906121a0191b0faba501bf9d0b5eab1040d356
josmet52 <joseph.metrailler@bluewin.ch>
2018-02-14 22:38:42 +0100

    20180214 bash ok

--- install_jo_config.sh
+++ install_jo_config.sh
@@ -61,13 +61,12 @@ cd ..
 echo
 
 echo configure mysql
-#sh /home/pi/Documents/projets_jo/piInstallJoConfig/configure_mysql.sh
 
 sudo mysql -u root -e "SET PASSWORD FOR root@localhost = PASSWORD('mablonde');"
 sudo mysql -u root -pmablonde -e "DROP USER IF EXISTS pi@localhost;"
 sudo mysql -u root -pmablonde -e "CREATE USER pi@localhost IDENTIFIED BY 'mablonde';"
 sudo mysql -u root -pmablonde -e "GRANT ALL PRIVILEGES ON *.* TO pi@localhost;"
 sudo mysql -u root -pmablonde -e "CREATE DATABASE tlogger;"
-sudo mysql -u root -pmablonde < /home/pi/Documents/projets_jo/tempLogger/mysql/create_users_db_and_tables.sql
+sudo mysql -u root -pmablonde < /home/pi/Documents/piInstallJoConfig/create_users_db_and_tables.sql
 
 
