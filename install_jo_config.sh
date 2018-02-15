07e9ac277b8bfc23aa16be586c1e64a4e7261d14
josmet42 <joseph.metrailler@bluewin.ch>
2018-02-15 11:15:51 +0100

    20180215 version testee et completee

--- install_jo_config.sh
+++ install_jo_config.sh
@@ -70,4 +70,26 @@ sudo mysql -u root -pmablonde -e "GRANT ALL PRIVILEGES ON *.* TO pi@localhost;"
 sudo mysql -u root -pmablonde -e "CREATE DATABASE tlogger;"
 sudo mysql -u root -pmablonde < /home/pi/Documents/projets_jo/tempLogger/mysql/create_users_db_and_tables.sql
 
+echo install gnome-schedule
+sudo apt-get -y install gnome-schedule
+echo
+
+echo install cups (printer queue)
+sudo apt-get -y install cups
+sudo usermod -a -G lpadmin pi
+sudo adduser pi lp
+echo
+
+echo dernières mises à jour
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo apt-get -y autoremove
+echo
+
+echo installation à la mode jo terminée
+echo pour installer l'imprimante lancer l'explorer avec l'adresse
+echo 127.0.0.1:631 
+echo
+
 
