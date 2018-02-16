3d73f9837fda30c62e697c3d9f4935bd21f25032
josmet52 <joseph.metrailler@bluewin.ch>
2018-02-16 16:08:20 +0100

    Delete configure_mysql.sh

--- configure_mysql.sh
+++
@@ -1,6 +0,0 @@
-sudo mysql -u root -e "SET PASSWORD FOR root@localhost = PASSWORD('mablonde');"
-sudo mysql -u root -pmablonde -e "DROP USER IF EXISTS pi@localhost;"
-sudo mysql -u root -pmablonde -e "CREATE USER pi@localhost IDENTIFIED BY 'mablonde';"
-sudo mysql -u root -pmablonde -e "GRANT ALL PRIVILEGES ON *.* TO pi@localhost;"
-sudo mysql -u root -pmablonde -e "CREATE DATABASE tlogger;"
-sudo mysql -u root -pmablonde < /home/pi/Documents/projets_jo/tempLogger/mysql/create_users_db_and_tables.sql
