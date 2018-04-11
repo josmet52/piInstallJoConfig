60751c6f3fca18336a144928b83215285c7423b2
josmet52 <joseph.metrailler@bluewin.ch>
2018-04-11 12:19:13 +0200

    20180411_1219

---
+++ install_mysql.sh
@@ -0,0 +1,37 @@
+﻿#!/bin/bash
+echo ----- install mysql -----
+echo
+
+echo ------ installing mysql-server -----
+echo
+sudo apt-get -y install mysql-server
+
+echo
+echo ----- install mysql-client -----
+echo
+sudo apt-get -y install mysql-client
+
+echo
+echo ----- install python-mysqldb -----
+echo
+sudo apt-get -y install python-mysqldb
+
+echo
+echo ----- install python-pymysql-----
+echo
+sudo apt-get install python3-pymysql
+
+echo
+echo ----- configure mysql -----
+echo
+sudo mysql -u root -e "SET PASSWORD FOR root@localhost = PASSWORD('mablonde');"
+sudo mysql -u root -pmablonde -e "DROP USER IF EXISTS pi@localhost;"
+sudo mysql -u root -pmablonde -e "CREATE USER pi@localhost IDENTIFIED BY 'mablonde';"
+sudo mysql -u root -pmablonde -e "GRANT ALL PRIVILEGES ON *.* TO pi@localhost;"
+
+echo
+echo ----- installation à la mode jo terminée ------
+echo
+
+
+
